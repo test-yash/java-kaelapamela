bba89a98eeb37c3fd97edd42afb4546cff0ed653
kaelapamela <77412001+kaelapamela@users.noreply.github.com>
2021-03-09 17:24:24 +0700

    Add files via upload

---
+++ test.java
@@ -0,0 +1,13 @@
+package WILDAN_TECHNO_ART;
+
+import java.util.Scanner;
+
+public class latihan_java {
+    public static void main(String[] args){
+        //Membuat Instance Scanner
+       
+            
+            //Membuat Baris Baru
+            System.out.println("hello");
+    }
+}
